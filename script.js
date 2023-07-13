0b9ee28cd1ad1eb673469948be4ae181ac784e1e
RonOvski1212 <245785gio@gmail.com>
2023-07-14 00:20:23 +0400

    bugs

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ const chekwin = () => {
       return true;
     }
   }
-
   for (let col = 0; col < 3; col++) {
     if (
       gameBoard[0][col] === currentPlayer &&
@@ -24,21 +23,78 @@ const chekwin = () => {
     ) {
       return true;
     }
-    if (
-      gameBoard[0][0] === currentPlayer &&
-      gameBoard[1][1] === currentPlayer &&
-      gameBoard[2][2] === currentPlayer
-    ) {
-      return true;
-    }
-
-    if (
-      gameBoard[0][2] === currentPlayer &&
-      gameBoard[1][1] === currentPlayer &&
-      gameBoard[2][0] === currentPlayer
-    ) {
-      return true;
-    }
+  }
+  if (
+    gameBoard[0][0] === currentPlayer &&
+    gameBoard[1][1] === currentPlayer &&
+    gameBoard[2][2] === currentPlayer
+  ) {
+    return true;
+  }
+  if (
+    gameBoard[0][2] === currentPlayer &&
+    gameBoard[1][1] === currentPlayer &&
+    gameBoard[2][0] === currentPlayer
+  ) {
+    return true;
   }
   return false;
 };
+const checkDraw = () => {
+  for (let row = 0; row < 3; row++) {
+    for (let col = 0; col < 3; col++) {
+      if (gameBoard[row][col] === "") {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+const makeMove = (row, col) => {
+  const info = document.querySelector(".info");
+  if (gameBoard[row][col] === "") {
+    gameBoard[row][col] = currentPlayer;
+
+    if (checkWin()) {
+      info.innerText = `Player ${currentPlayer} win!`;
+      resetGame();
+    } else if (checkDraw()) {
+      info.innerText = "Draw!";
+      resetGame();
+    } else {
+      currentPlayer = currentPlayer === "x" ? "o" : "x";
+    }
+  }
+};
+
+const resetGame = () => {
+  currentPlayer = "x";
+  gameBoard = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  renderBoard();
+};
+const renderBoard = () => {
+  const board = doqument.querySelector(".board");
+  board.innerHTML = "";
+
+  for (let row = 0; row < 3; row++) {
+    for (let col = 0; col < 3; col++) {
+      const cell = document.createElement("div");
+      cell.className = "cell";
+      cell.innerText = gameBoard[row][col];
+
+      cell.addEventListener("click", () => {
+        makeMove(row, col);
+        renderBoard();
+      });
+      board.appendChild(cell);
+    }
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderBoard();
+});
