cb1db587adb533a8ab05f892ab8a1a2944d911c1
RonOvski1212 <121582146+RonOvski1212@users.noreply.github.com>
2023-10-22 01:07:36 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,100 +1,112 @@
-let currentPlayer = "x";
-let gameBoard = [
-  ["", "", ""],
-  ["", "", ""],
-  ["", "", ""],
-];
+const board = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let gameEnded = false;
+let winsX = 0;
+let winsO = 0;
 
-const checkWin = () => {
-  for (let row = 0; row < 3; row++) {
-    if (
-      gameBoard[row][0] === currentPlayer &&
-      gameBoard[row][1] === currentPlayer &&
-      gameBoard[row][2] === currentPlayer
-    ) {
-      return true;
+function play(index) {
+  if (gameEnded) {
+    return;
+  }
+
+  const resultElement = document.getElementById("result");
+
+  if (board[index] === "") {
+    board[index] = currentPlayer;
+
+    const cell = document.getElementById(`cell${index}`);
+    cell.innerText = currentPlayer;
+    cell.style.pointerEvents = "none";
+
+    if (currentPlayer === "X") {
+      cell.style.background =
+        "linear-gradient(to bottom right, #FF0000, #FFA500)";
+    } else {
+      cell.style.background =
+        "linear-gradient(to bottom right, #0000FF, #00FF00)";
     }
-  }
-  for (let col = 0; col < 3; col++) {
-    if (
-      gameBoard[0][col] === currentPlayer &&
-      gameBoard[1][col] === currentPlayer &&
-      gameBoard[2][col] === currentPlayer
-    ) {
-      return true;
-    }
-  }
-  if (
-    gameBoard[0][0] === currentPlayer &&
-    gameBoard[1][1] === currentPlayer &&
-    gameBoard[2][2] === currentPlayer
-  ) {
-    return true;
-  }
-  if (
-    gameBoard[0][2] === currentPlayer &&
-    gameBoard[1][1] === currentPlayer &&
-    gameBoard[2][0] === currentPlayer
-  ) {
-    return true;
-  }
-  return false;
-};
-const checkDraw = () => {
-  for (let row = 0; row < 3; row++) {
-    for (let col = 0; col < 3; col++) {
-      if (gameBoard[row][col] === "") {
-        return false;
-      }
-    }
-  }
-  return true;
-};
-const makeMove = (row, col) => {
-  const info = document.querySelector(".info");
-  if (gameBoard[row][col] === "") {
-    gameBoard[row][col] = currentPlayer;
 
     if (checkWin()) {
-      info.innerText = `Player ${currentPlayer} win!`;
-      resetGame();
-    } else if (checkDraw()) {
-      info.innerText = "Draw!";
-      resetGame();
-    } else {
-      currentPlayer = currentPlayer === "x" ? "o" : "x";
-    }
-  }
-};
+      resultElement.innerText = `Игрок ${currentPlayer} победил!`;
+      disableCells();
+      gameEnded = true;
 
-const resetGame = () => {
-  currentPlayer = "x";
-  gameBoard = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
+      if (currentPlayer === "X") {
+        winsX++;
+        document.getElementById("winsX").innerText = `Побед X: ${winsX}`;
+      } else {
+        winsO++;
+        document.getElementById("winsO").innerText = `Побед O: ${winsO}`;
+      }
+
+      return;
+    }
+
+    if (checkDraw()) {
+      resultElement.innerText = "Ничья!";
+      gameEnded = true;
+      return;
+    }
+
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    document.getElementById(
+      "player"
+    ).innerText = `Ход игрока: ${currentPlayer}`;
+  } else {
+    resultElement.innerText = "Эта ячейка уже занята. Выберите другую.";
+  }
+}
+
+function checkWin() {
+  const winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
   ];
-  renderBoard();
-};
-const renderBoard = () => {
-  const board = doqument.querySelector(".board");
-  board.innerHTML = "";
 
-  for (let row = 0; row < 3; row++) {
-    for (let col = 0; col < 3; col++) {
-      const cell = document.createElement("div");
-      cell.className = "cell";
-      cell.innerText = gameBoard[row][col];
-
-      cell.addEventListener("click", () => {
-        makeMove(row, col);
-        renderBoard();
-      });
-      board.appendChild(cell);
+  for (const pattern of winPatterns) {
+    const [a, b, c] = pattern;
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return true;
     }
   }
-};
 
-document.addEventListener("DOMContentLoaded", () => {
-  renderBoard();
-});
+  return false;
+}
+
+function checkDraw() {
+  return !board.includes("");
+}
+
+function disableCells() {
+  const cells = document.getElementsByClassName("cell");
+  for (const cell of cells) {
+    cell.style.pointerEvents = "none";
+  }
+}
+
+function restartGame() {
+  board.fill("");
+  const cells = document.getElementsByClassName("cell");
+  for (const cell of cells) {
+    cell.innerText = "";
+    cell.style.pointerEvents = "auto";
+    cell.style.background = "";
+  }
+
+  document.getElementById("player").innerText = "Ход игрока: X";
+  document.getElementById("result").innerText = "";
+  gameEnded = false;
+}
+
+document.getElementById("restartButton").addEventListener("click", restartGame);
+
+const cells = document.getElementsByClassName("cell");
+for (let i = 0; i < cells.length; i++) {
+  cells[i].addEventListener("click", () => play(i));
+}
