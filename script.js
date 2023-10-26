e5e5e13904def22ce6813a48da14edfd1d8f67c2
RonOvski1212 <245785gio@gmail.com>
2023-10-26 07:40:54 +0400

    added bot and optimisation

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ let currentPlayer = "X";
 let gameEnded = false;
 let winsX = 0;
 let winsO = 0;
+let gameMode = "playerVsPlayer";
+let botPlayer = "O";
+
+function setGameMode(mode) {
+  gameMode = mode;
+  restartGame();
+}
 
 function play(index) {
   if (gameEnded) {
@@ -38,23 +45,102 @@ function play(index) {
         winsO++;
         document.getElementById("winsO").innerText = `Побед O: ${winsO}`;
       }
-
-      return;
-    }
-
-    if (checkDraw()) {
+    } else if (checkDraw()) {
       resultElement.innerText = "Ничья!";
       gameEnded = true;
-      return;
-    }
+    } else {
+      currentPlayer = currentPlayer === "X" ? "O" : "X";
+      document.getElementById(
+        "player"
+      ).innerText = `Ход игрока: ${currentPlayer}`;
 
-    currentPlayer = currentPlayer === "X" ? "O" : "X";
-    document.getElementById(
-      "player"
-    ).innerText = `Ход игрока: ${currentPlayer}`;
+      if (gameMode === "playerVsBot" && currentPlayer === botPlayer) {
+        setTimeout(botPlay, 1000);
+      }
+    }
   } else {
     resultElement.innerText = "Эта ячейка уже занята. Выберите другую.";
   }
+}
+
+function botPlay() {
+  if (currentPlayer === botPlayer) {
+    let bestMove = findBestMove();
+    if (bestMove !== undefined) {
+      play(bestMove);
+    }
+  }
+}
+
+function findBestMove() {
+  let bestMove;
+  let bestScore = -Infinity;
+
+  for (let i = 0; i < 9; i++) {
+    if (board[i] === "") {
+      board[i] = botPlayer;
+      let score = minimax(board, 0, false);
+      board[i] = "";
+
+      if (score > bestScore) {
+        bestScore = score;
+        bestMove = i;
+      }
+    }
+  }
+
+  return bestMove;
+}
+
+function minimax(board, depth, isMaximizing) {
+  let scores = {
+    X: -1,
+    O: 1,
+    draw: 0,
+  };
+
+  if (checkWin()) {
+    return scores[currentPlayer] - depth;
+  } else if (checkDraw()) {
+    return scores["draw"];
+  }
+
+  let bestScore, bestMove;
+
+  if (isMaximizing) {
+    bestScore = -Infinity;
+    for (let i = 0; i < 9; i++) {
+      if (board[i] === "") {
+        board[i] = botPlayer;
+        let score = minimax(board, depth + 1, false);
+        board[i] = "";
+
+        if (score > bestScore) {
+          bestScore = score;
+          bestMove = i;
+        }
+      }
+    }
+  } else {
+    bestScore = Infinity;
+    for (let i = 0; i < 9; i++) {
+      if (board[i] === "") {
+        board[i] = currentPlayer;
+        let score = minimax(board, depth + 1, true);
+        board[i] = "";
+        if (score < bestScore) {
+          bestScore = score;
+          bestMove = i;
+        }
+      }
+    }
+  }
+
+  if (depth === 0) {
+    return bestMove;
+  }
+
+  return bestScore;
 }
 
 function checkWin() {
@@ -102,6 +188,10 @@ function restartGame() {
   document.getElementById("player").innerText = "Ход игрока: X";
   document.getElementById("result").innerText = "";
   gameEnded = false;
+
+  if (gameMode === "playerVsBot" && currentPlayer === botPlayer) {
+    setTimeout(botPlay, 1000);
+  }
 }
 
 document.getElementById("restartButton").addEventListener("click", restartGame);
